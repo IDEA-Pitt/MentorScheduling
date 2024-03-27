38294199b375323be5be223d8422dc93e2938946
ColeMinersUnion <colehansen018@gmail.com>
2024-03-27 10:45:21 -0400

    made some list comprehensions to add stuff to the hours/mentors etc

--- MentorScheduler/Hour.py
+++ MentorScheduler/Hour.py
@@ -1,8 +1,12 @@
+from Mentor import Mentor
 class Hour:
-    def __init__(self, possible, final):
+    def __init__(self, time, possible=[], final=0):
+        self.time = time
         self.possibleMentors = possible
-        self.current = 0 #3 mentors per hour
+        self.current = final #3 mentors per hour
     
     def length(self):
         return self.possibleMentors.length() #sees which hours need to be scheduled first
     
+    def add(self, mentor):
+        self.possibleMentors.append(mentor)
--- MentorScheduler/main.py
+++ MentorScheduler/main.py
@@ -1,6 +1,26 @@
 from Mentor import Mentor
 from Hour import Hour #two objects to mixmatch
 from Data import getData #funciton
-#get data from google forms here
+
+
+DaysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+OpenHours = {}
+for day in DaysOfTheWeek:
+    OpenHours[day] = [Hour(x) for x in range(1, 12)]
+    #Creats hours from 8-7 for each day
+    #Will filter in the mentors for each day later
+
+#getting the data from the downloaded excel sheet
 mentors = getData()
+
+#time to filter the mentors into the hours
+#for each of the mentors. Now look through each day and hour to see if their available
+for day in DaysOfTheWeek:
+    for x in range(1, 12):
+        #list comprehensions go crazy
+        OpenHours[day][x] = [peep for peep in mentors if peep.available(day, x)]
+        
+
+
+
 print(len(mentors))
